02a70762c1ef49949d5e0e6f8302588d208ebde7
KhoaPham <phamdangkhoa0403@gmail.com>
2018-12-05 22:48:58 +0700

    Add Edit Delete for Plan Bucket Task , Task assignee

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        cap21t12Entities db = new cap21t12Entities();
+
         public PartialViewResult Index()
         {
-            return PartialView();
+            var task = db.tasks.ToList().Where(p => p.AssigneeID == User.Identity.GetUserId());
+
+            return PartialView(task);
         }
 
         public ActionResult About()
