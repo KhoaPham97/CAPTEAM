e9a2c4177a0d921c6ba978c492479235bbf2f176
KhoaPham <phamdangkhoa0403@gmail.com>
2018-12-05 21:47:52 +0700

    Add conection string , logoff button

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace WebApplication.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public PartialViewResult Index()
